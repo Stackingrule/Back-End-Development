2da13e8776d84ec5c62d25442451aba62df60a13
Stackingrule <messageswitching@gmail.com>
2019-12-28 22:56:25 +0800

    add Scala COllection Library

---
+++ Scala-Learn/learn-scala-from-scratch/04-Scala-Collection-Library/02-An-Introduction-to-the-Collection-Library.scala
@@ -0,0 +1,23 @@
+val seqCollection = Seq(2,4,6,8)
+
+val result = seqCollection.apply(1)
+
+// Driver Code
+print(result)
+
+
+val setCollection = Set("apple", "orange", "banana", "grape")
+
+val result = setCollection.apply("orange")
+
+// Driver Code
+print(result)
+
+
+
+val mapCollection = Map(("a",25),("b",50),("c",75))
+
+val result = mapCollection.apply("c")
+
+// Driver Code
+print(result)
