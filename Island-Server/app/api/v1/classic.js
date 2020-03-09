dad424a64c0523a25e08303a3c9feed409e47945
Stackingrule <messageswitching@gmail.com>
2020-03-09 23:14:04 +0800

    定义异常返回格式

--- Island-Server/app/api/v1/classic.js
+++ Island-Server/app/api/v1/classic.js
@@ -8,11 +8,19 @@ router.post('/v1/:id/classic/latest', (ctx, next) => {
     const query = ctx.request.query
     const headers = ctx.request.header
     const body = ctx.request.body
+    if (true) {
+        const error = new Error('错误')
+        error.errorCode = 10001
+        error.status = 400
+        error.requestUrl = `${ctx.method} ${ctx.path}`
+        throw error
+        // error_code
+    }
+
     ctx.body = {
       key: 'classic'
     }
 
-    throw new Error('API Exception')
 })
 
 module.exports = router
--- Island-Server/middlewares/exception.js
+++ Island-Server/middlewares/exception.js
@@ -2,6 +2,14 @@ const catchError = async (ctx, next) => {
     try {
         await next()
     } catch (error) {
+        if (error.errorCode) {
+            ctx.body = {
+                msg: error.message,
+                error_code: error.errorCode,
+                request: error.requestUrl
+            }
+            ctx.status = error.status
+        }
         ctx.body = '服务器有的问题，稍等片刻！'
     }
 }
