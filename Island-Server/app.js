fa49cff5c38dbb7e431d68b04c1fe75b222678dd
Stackingrule <messageswitching@gmail.com>
2020-03-09 12:05:53 +0800

    初始化管理器与Process.cwd()

--- Island-Server/app.js
+++ Island-Server/app.js
@@ -1,18 +1,10 @@
 const Koa = require('koa')
-const requireDirectory = require('require-directory')
-const Router = require('koa-router')
+
+const InitManager = require('./core/init')
 
 const app = new Koa();
-
-requireDirectory(module, './api', {
-    visit: whenLoadModule
-})
-
-function whenLoadModule(obj) {
-    if (obj instanceof Router) {
-        app.use(obj.routes())
-    }
-}
+console.log(process.cwd())
+InitManager.initCore(app)
 
 // app.use(book.routes());
 // app.use(classic.routes());
